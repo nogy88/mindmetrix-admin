54062d28e5aa6975802a93c431f6078da739451a
Norovsuren <norovsuren@tanasoft.mn>
2022-11-28 10:57:29 +0800

    removed /api

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: process.env.NODE_ENV === "production" ? "/api/" : "/",
+  baseURL: process.env.NODE_ENV === "production" ? "/" : "/",
 });
 
 export const authRequest = (endpoint, payload) => {
